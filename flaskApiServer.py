1e5b095638137fb7ad7ecc8834ee6a1ebaab6737
Ian <ioloughlin.ct.oem@gmail.com>
2018-07-01 20:33:50 -0400

    added GET with single parameter

--- flaskApiServer.py
+++ flaskApiServer.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Simple Get with a single parameter.
+class HelloWorld(Resource):
+    def get(self, cid):
+        return {'cid': cid}
+
+
+api.add_resource(HelloWorld, '/api/customer/<int:cid>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
